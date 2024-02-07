725e4d5207e06300ab5ac91eac64b22d33791c40
egaiosowor <egaiosowor@yahoo.com>
2024-02-07 15:14:45 +0100

    fix industry page header

--- src/Pages/Industry/Header.jsx
+++ src/Pages/Industry/Header.jsx
@@ -2,7 +2,7 @@ export default function Header({title, industry}){
 
     return(
         <section className={`grid justify-center items-center bg-${industry} h-[550px] bg-no-repeat bg-cover`} >
-                <h1 className="font-bold text-white text-[45px] lg:text-[85px]" >{title.toUpperCase()}</h1>
+                <h1 className="text-center font-bold text-white text-[45px] lg:text-[85px]" >{title.toUpperCase()}</h1>
         </section>
     )
 }
